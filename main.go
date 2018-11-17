3f164627b636aa348b0f6dafb506c36386ade020
hirsch <gery.hirschfeld@w3tec.ch>
2018-11-17 17:40:37 +0100

    :tada: Initial commit

--- main.go
+++ main.go
@@ -3,17 +3,13 @@ package main
 import (
 	"github.com/hirsch88/quick-tournament-api-go/app"
 	"github.com/joho/godotenv"
-	"github.com/sirupsen/logrus"
 
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
 	// Loads the env variables
-	err := godotenv.Load()
-	if err != nil {
-		logrus.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 
 	app.Bootstrap()
 }
